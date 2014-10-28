73575d20b9bf2aef90107a21ecd6ca1d68df1bb1
QLKang <jcln.chase@gmail.com>
2014-10-28 00:17:53 -0700

    Single linked list
    Create a single linked list

---
+++ SingleLinkedList.java
@@ -0,0 +1,37 @@
+/*create a single linked list*/
+
+public class SingleLinkedList {
+	SingleLinkedList next = null;
+	int item = 0;
+	
+	
+	public void appendToTail(int n){
+		SingleLinkedList end = new SingleLinkedList();
+		end.item =n;
+		SingleLinkedList current = this;
+		while(current.next!=null){
+			current = current.next;
+		}
+		current.next = end;
+	}
+	
+	
+// for test: appendToTail works well	
+	public static void main(String[] args){
+		SingleLinkedList ln = new SingleLinkedList();
+		ln.item=4;
+		ln.appendToTail(5);
+		ln.appendToTail(6);
+		ln.appendToTail(7);
+		SingleLinkedList runner = ln;
+		while(runner!=null){
+			System.out.print("-->"+runner.item);
+			runner = runner.next;
+		}
+		
+		
+		
+	}
+	
+
+}
